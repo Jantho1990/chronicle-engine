ead7eec51e9c80a3c2d20df2d75483aaed4220e6
Joshua Anthony <jantho1990@gmail.com>
2018-02-10 16:23:46 -0600

    added Page and Link handlers

---
+++ src/components/handlers/LinkHandler.vue
@@ -0,0 +1,32 @@
+<script>
+  import { EventBus } from '../../components/events/EventBus'
+
+  export default {
+    components: {
+      EventBus
+    },
+    created: function () {
+      EventBus.$on('storeLink', this.storeLink)
+      EventBus.$on('updateLink', this.updateLink)
+      EventBus.$on('destroyLink', this.destroyLink)
+    },
+    data () {
+      return {
+        links: []
+      }
+    },
+    methods: {
+      storeLink: function (link) {
+        this.links.push(link)
+      },
+      updateLink: function (link) {
+        let i = this.links.findIndex(_link => _link.id === link.id)
+        this.links[i] = link
+      },
+      destroyLink: function (id) {
+        let i = this.links.findIndex(_link => _link.id === id)
+        this.links.splice(i, 1)
+      }
+    }
+  }
+</script>
---
+++ src/components/handlers/PageHandler.vue
@@ -0,0 +1,32 @@
+<script>
+  import { EventBus } from '../../components/events/EventBus'
+
+  export default {
+    components: {
+      EventBus
+    },
+    created: function () {
+      EventBus.$on('storePage', this.storePage)
+      EventBus.$on('updatePage', this.storePage)
+      EventBus.$on('destroyPage', this.storePage)
+    },
+    data () {
+      return {
+        pages: []
+      }
+    },
+    methods: {
+      storePage: function (page) {
+        this.pages.push(page)
+      },
+      updatePage: function (page) {
+        let i = this.pages.findIndex(_page => _page.id === page.id)
+        this.pages[i] = page
+      },
+      destroyPage: function (id) {
+        let i = this.pages.findIndex(page => page.id === id)
+        this.pages.splice(i, 1)
+      }
+    }
+  }
+</script>
